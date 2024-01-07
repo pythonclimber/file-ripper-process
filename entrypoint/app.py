a2402754654ddd1bf9f2baae3c4d47ce17e8708a
Aaron <asmitty92@gmail.com>
2024-01-07 10:27:46 -0600

    Add lambda authorizer

--- entrypoint/app.py
+++ entrypoint/app.py
@@ -1,7 +1,13 @@
 import cgi
+import os
 from io import BytesIO
+from json import loads
+import tempfile
 
-from chalice import Chalice
+from chalice import Chalice, AuthResponse
+from chalice.app import AuthRequest
+
+from file_ripper import rip_file, FileDefinition
 
 app = Chalice(app_name='entrypoint')
 
@@ -12,17 +18,38 @@ def _get_parts():
     _, parameters = cgi.parse_header(content_type)
     parameters['boundary'] = parameters['boundary'].encode('utf-8')
     parsed = cgi.parse_multipart(rfile, parameters)
-    return parsed['file'][0], parsed['fileDefinition'][0]
+    return parsed['file'][0], loads(parsed['fileDefinition'][0])
+
+
+def _create_file(file_data: bytes):
+    with open('current_file.txt', 'wt') as file:
+        file_contents = file_data.decode('utf-8')
+        print(f'file_contents: {file_contents}')
+        file.write(file_contents)
+
+
+@app.authorizer()
+def authorizer(auth_request: AuthRequest):
+    print(auth_request.token)
+    return AuthResponse(['/rip-file'], principal_id='user')
 
 
 @app.route(
     '/rip-file',
     methods=['POST'],
     content_types=['multipart/form-data'],
-    cors=True
+    cors=True,
+    authorizer=authorizer
 )
-def rip_file():
+def rip_file_web():
     file_data, file_definition = _get_parts()
-    print(file_data)
-    print(file_definition)
-    return {'hello': 'from rip-files'}
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        _create_file(file_data)
+
+        with open('current_file.txt', 'rt') as file:
+            file_instance = rip_file(file, FileDefinition.create_from_dict(file_definition))
+            print(f'file_instance: {len(file_instance)}')
+
+    return file_instance.to_dict()
